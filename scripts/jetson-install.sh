4205be646c88968dcd60d441d0734b63a5ce81f1
fstg <ciditworld@gmail.com>
2021-04-19 12:59:29 -0400

    added requirements

---
+++ scripts/jetson-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bahs
+
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes ((CAUSES FAIL))
+IFS=$'\t\n'
+
+main() {
+
+}
+
+apt_installation() {
+    
+}
+
+git_setup() {
+
+}
+
+main "$@"
